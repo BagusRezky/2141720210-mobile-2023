2be51092d25a579f4d22bc52a8cb122dfeadb757
BagusRezky <bagusrezky15@gmail.com>
2023-09-04 11:31:18 +0700

    Soal 1

---
+++ week-02/src/soal1.dart
@@ -0,0 +1,10 @@
+void main() {
+  // Code before modify
+  for (var i = 0; i < 10; i++) {
+     print('hello ${i + 2}');
+   }
+   // code after modify
+  for (var i = 18; i >= 9; i--) {
+    print('Nama saya adalah Fulan, sekarang berumur $i');
+  }
+}
