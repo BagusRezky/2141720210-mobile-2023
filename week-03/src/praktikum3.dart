d55bffad5af38ec77d8d2551e5ae6efebeeddd45
BagusRezky <bagusrezky15@gmail.com>
2023-09-11 10:45:41 +0700

    Update Praktikum 3

--- week-03/src/praktikum3.dart
+++ week-03/src/praktikum3.dart
@@ -1,5 +1,11 @@
 void main(){
-for (int index = 10; index < 27; index++) {
-    print("Index");
+  for (int index = 0; index < 27; index++) {
+    if (index == 21) {
+      print(index);
+      break;
+    } else if (index > 1 || index < 7) {
+      print(index);
+      continue;
+    }
   }
 }
