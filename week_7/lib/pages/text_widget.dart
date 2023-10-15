499f875f0bdac6caa548b3d939f7cfe71034a418
BagusRezky <bagusrezky15@gmail.com>
2023-10-15 20:09:25 +0700

    [Add] TextWidget

---
+++ week_7/lib/pages/text_widget.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Txt extends StatelessWidget {
+  const Txt({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      crossAxisAlignment: CrossAxisAlignment.baseline,
+      textBaseline: TextBaseline.alphabetic,
+      children: [
+        Text(
+          'Hey!',
+          style: TextStyle(
+            fontSize: 30,
+            fontFamily: 'Futura',
+            color: Colors.blue,
+          ),
+        ),
+        Text(
+          'Hey!',
+          style: TextStyle(
+            fontSize: 50,
+            fontFamily: 'Futura',
+            color: Colors.green,
+          ),
+        ),
+        Text(
+          'Hey!',
+          style: TextStyle(
+            fontSize: 40,
+            fontFamily: 'Futura',
+            color: Colors.red,
+          ),
+        ),
+      ],
+    );
+  }
+}
