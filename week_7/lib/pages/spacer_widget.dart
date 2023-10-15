1eacf1f643745ccf56b204df4ffaaa79485608f2
BagusRezky <bagusrezky15@gmail.com>
2023-10-15 10:54:19 +0700

    [Add] spacer

---
+++ week_7/lib/pages/spacer_widget.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class Sp extends StatelessWidget {
+  const Sp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget spacer = const Row(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        BlueBox(),
+        Spacer(
+          flex: 1,
+        ),
+        BlueBox(),
+      ],
+    );
+
+    return Scaffold(
+      body: ListView(
+        children: [
+          spacer,
+        ],
+      ),
+    );
+  }
+}
+
+class BlueBox extends StatelessWidget {
+  const BlueBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      height: 50,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        border: Border.all(),
+      ),
+    );
+  }
+}
