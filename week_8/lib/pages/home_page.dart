e34ffe9c2c2540d4057d04513401fc377717f0af
BagusRezky <bagusrezky15@gmail.com>
2023-10-22 22:49:06 +0700

    [Changes] Home page

--- week_8/lib/pages/home_page.dart
+++ week_8/lib/pages/home_page.dart
@@ -1,46 +1,114 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:week_8/models/item.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
   final List<Item> items = [
-    Item(name: 'Sugar', price: 5000),
-    Item(name: 'Salt', price: 2000),
+    Item(
+      name: 'Burger Malleta',
+      price: 9000,
+      imageUrl: 'assets/burger.png',
+      stock: 50,
+      rating: 4.5,
+    ),
+    Item(
+      name: 'Drink',
+      price: 9000,
+      imageUrl: 'assets/drink.png',
+      stock: 50,
+      rating: 4.5,
+    ),
   ];
   final routeName = '/item';
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(8),
-      child: ListView.builder(
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          final item = items[index];
-          return Material(
-            child: InkWell(
-              onTap: () {
-                Navigator.pushNamed(context, routeName, arguments: item);
-              },
-              child: Card(
-                child: Container(
-                  margin: const EdgeInsets.all(8),
-                  child: Row(
-                    children: [
-                      Expanded(child: Text(item.name.toString())),
-                      Expanded(
-                        child: Text(
-                          item.price.toString(),
-                          textAlign: TextAlign.end,
-                        ),
-                      )
-                    ],
-                  ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(8),
+        child: Column(
+          children: [
+            Center(
+              child: Text(
+                'My Shopping Cart',
+                style: GoogleFonts.poppins(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w600,
+                  color: const Color(0xff191919),
                 ),
               ),
             ),
-          );
-        },
+            const SizedBox(
+                height: 16), // Add some spacing between the text and ListView
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.all(8),
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  final item = items[index];
+                  return InkWell(
+                    onTap: () =>
+                        Navigator.pushNamed(context, '/item', arguments: item),
+                    child: Card(
+                      child: Container(
+                        margin: const EdgeInsets.all(8),
+                        child: Row(
+                          children: [
+                            Expanded(child: Text(item.name.toString())),
+                            Expanded(
+                              child: Text(
+                                'Rp ${item.price.toString()}',
+                                textAlign: TextAlign.end,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.blue,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: const EdgeInsets.only(
+                top: 15.0,
+                left: 30,
+              ),
+              child: Text(
+                'Upgrade Now',
+                style: GoogleFonts.poppins(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                  color: const Color(0xffFFFFFF),
+                ),
+              ),
+            ),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: const EdgeInsets.only(top: 15.0, left: 70),
+              child: Image.asset(
+                'assets/rowIcon.png',
+                width: 24,
+              ),
+            ),
+            label: '',
+          ),
+        ],
       ),
     );
   }
