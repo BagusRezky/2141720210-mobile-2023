e4db31d1a0d6683380ecfbde07647d38a3899d16
BagusRezky <bagusrezky15@gmail.com>
2023-10-22 22:48:07 +0700

    [Add] Theme

---
+++ week_8/lib/widgets/theme.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color blackColor = const Color(0xff0E1954);
+TextStyle boldTextStyle = GoogleFonts.poppins(
+  color: blackColor,
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle lightTextStyle = GoogleFonts.poppins(
+  color: blackColor,
+  fontSize: 15,
+);
+
+TextStyle buttonTextStyle = GoogleFonts.poppins(
+  color: const Color(0xffF8F8F8),
+  fontSize: 18,
+  fontWeight: FontWeight.w600,
+);
+
+//Day 5
+Color whiteColor = const Color(0xffffffff);
+TextStyle titleTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+  fontSize: 24,
+  fontWeight: FontWeight.w600,
+);
+
+TextStyle priceTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+  fontSize: 24,
+);
+
+TextStyle askTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+  fontSize: 18,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle button2TextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+  fontSize: 16,
+  fontWeight: FontWeight.w600,
+);
+
+//Day 6
+Color black2Color = const Color(0xff191919);
+TextStyle title2TextStyle = GoogleFonts.poppins(
+  color: black2Color,
+  fontSize: 22,
+  fontWeight: FontWeight.w600,
+);
