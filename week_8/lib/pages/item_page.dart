d56f94bf32d7de9cba762a4a35d779aa8f4f0125
BagusRezky <bagusrezky15@gmail.com>
2023-10-22 22:48:24 +0700

    [Add] Item Page

--- week_8/lib/pages/item_page.dart
+++ week_8/lib/pages/item_page.dart
@@ -1,19 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:week_8/models/item.dart';
+import 'package:week_8/widgets/theme.dart';
 
 class ItemPage extends StatelessWidget {
   const ItemPage({super.key});
-
   @override
   Widget build(BuildContext context) {
     final itemArgs = ModalRoute.of(context)!.settings.arguments as Item;
-    return Container(
-      margin: const EdgeInsets.all(8),
-      child: Card(
-        child: Row(
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: const Text('Item Page'),
+      ),
+      body: Container(
+        margin: const EdgeInsets.all(15),
+        child: Column(
           children: [
-            Text(itemArgs.name.toString()),
-            Text(itemArgs.price.toString())
+            Hero(
+              tag: itemArgs.name,
+              child: Image.asset(
+                itemArgs.imageUrl,
+                width: 500,
+                height: 500,
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.blue,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(itemArgs.name.toString(), style: boldTextStyle),
+                  const SizedBox(height: 8),
+                  Row(
+                    children: [
+                      Text(
+                        'Stok: ',
+                        style: buttonTextStyle,
+                      ),
+                      Text(
+                        itemArgs.stock.toString(),
+                        style: buttonTextStyle,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    children: [
+                      const Text(
+                        'Rating: ',
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                      const Icon(
+                        Icons.star,
+                        color: Colors.yellow, // Warna bintang kuning
+                        size: 24,
+                      ),
+                      Text(
+                        itemArgs.rating.toString(),
+                        style: buttonTextStyle,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
