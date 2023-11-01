053f8e7129eee358582194759cc5de95a5453a9d
BagusRezky <bagusrezky15@gmail.com>
2023-11-01 15:35:02 +0700

    [Add] ShowDebug

--- week_8/lib/main.dart
+++ week_8/lib/main.dart
@@ -12,6 +12,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
         '/': (context) => HomePage(),
