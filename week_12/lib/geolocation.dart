96d04d81fb9e4cd37ab875e976315da9d4d402d4
BagusRezky <bagusrezky15@gmail.com>
2023-11-19 18:30:47 +0700

    [Fix] Bug

--- week_12/lib/geolocation.dart
+++ week_12/lib/geolocation.dart
@@ -1,5 +1,5 @@
-import 'package:geolocator/geolocator.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LocationScreen extends StatefulWidget {
   const LocationScreen({super.key});
@@ -10,35 +10,69 @@ class LocationScreen extends StatefulWidget {
 
 class _LocationScreenState extends State<LocationScreen> {
   String myPosition = '';
+  Future<Position>? position;
+
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   getPosition().then((Position myPos) {
+  //     myPosition =
+  //         'Latitude: ${myPos.latitude.toString()} - Longitude: ${myPos.longitude.toString()}';
+  //     setState(() {
+  //       myPosition = myPosition;
+  //     });
+  //   });
+  // }
+
   @override
   void initState() {
     super.initState();
-    getPosition().then((Position myPos) {
-      myPosition =
-          'Latitude: ${myPos.latitude}, Longitude: ${myPos.longitude.toString()}';
-      setState(() {
-        myPosition = myPosition;
-      });
-    });
+    position = getPosition();
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   final myWidget =
+  //       myPosition == '' ? const CircularProgressIndicator() : Text(myPosition);
+  //   ;
+  //   return Scaffold(
+  //       appBar: AppBar(title: const Text('Current Location Lukas')),
+  //       body: Center(
+  //         child: Text(myPosition),
+  //       ));
+  // }
 
   @override
   Widget build(BuildContext context) {
-    final myWidget = myPosition == ''
-        ? const CircularProgressIndicator()
-        : Text(myPosition);
+    // final myWidget =
+    //     myPosition == '' ? const CircularProgressIndicator() : Text(myPosition);
+    // ;
     return Scaffold(
-      appBar: AppBar(title: const Text('current Location')),
-      body: Center(child: Text(myPosition)),
+      appBar: AppBar(title: const Text('Current Location Rezky')),
+      body: Center(
+          child: FutureBuilder(
+        future: position,
+        builder: (BuildContext context, AsyncSnapshot<Position> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              return const Text('Something terrible happened!');
+            }
+            return Text(snapshot.data.toString());
+          } else {
+            return const Text('');
+          }
+        },
+      )),
     );
   }
 
   Future<Position> getPosition() async {
-    await Geolocator.requestPermission();
+    // await Geolocator.requestPermission();
     await Geolocator.isLocationServiceEnabled();
-    Position? position = await Geolocator.getCurrentPosition();
+    await Future.delayed(const Duration(seconds: 3));
+    Position position = await Geolocator.getCurrentPosition();
     return position;
   }
-
-  
 }
