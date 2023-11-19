f397db30cf2145c204a61fef44ddc93482233a16
BagusRezky <bagusrezky15@gmail.com>
2023-11-19 18:34:58 +0700

    W12: S16

---
+++ week_12/lib/navigation_first.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import './navigation_second.dart';
+
+class NavigationFirst extends StatefulWidget {
+  const NavigationFirst({super.key});
+
+  @override
+  State<NavigationFirst> createState() => _NavigationFirstState();
+}
+
+class _NavigationFirstState extends State<NavigationFirst> {
+  Color color = Colors.blue.shade700;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: color,
+      appBar: AppBar(
+        title: const Text('Navigation First Screen Rezky'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            child: const Text('Change Color'),
+            onPressed: () {
+              _navigateAndGetColor(context);
+            }),
+      ),
+    );
+  }
+
+  Future _navigateAndGetColor(BuildContext context) async {
+    color = await Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const NavigationSecond()),
+        ) ??
+        Color(0xFFBFB29E);
+    setState(() {});
+  }
+}
---
+++ week_12/lib/navigation_second.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class NavigationSecond extends StatefulWidget {
+  const NavigationSecond({super.key});
+
+  @override
+  State<NavigationSecond> createState() => _NavigationSecondState();
+}
+
+class _NavigationSecondState extends State<NavigationSecond> {
+  @override
+  Widget build(BuildContext context) {
+    Color color;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Navigation Second Screen Rezky'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.grey.shade500,
+                ),
+                child: const Text('Yellow'),
+                onPressed: () {
+                  color = Color.fromARGB(255, 119, 134, 10);
+                  Navigator.pop(context, color);
+                }),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.grey.shade500,
+                ),
+                child: const Text('Brown'),
+                onPressed: () {
+                  color = Color.fromARGB(255, 215, 169, 0);
+                  Navigator.pop(context, color);
+                }),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.grey.shade500,
+                ),
+                child: const Text('Purple'),
+                onPressed: () {
+                  color = Color.fromARGB(255, 234, 0, 255);
+                  Navigator.pop(context, color);
+                }),
+          ],
+        ),
+      ),
+    );
+  }
+}
