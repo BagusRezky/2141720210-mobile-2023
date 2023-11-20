02651155389a3ab1363b9477f2f27cade548ec4e
BagusRezky <bagusrezky15@gmail.com>
2023-11-20 08:48:36 +0700

    W13: S2

---
+++ week-13/src/stream_rezky/lib/stream.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ColorStream {
+  final List<Color> color = [
+    Colors.blueGrey,
+    Colors.amber,
+    Colors.deepPurple,
+    Colors.lightBlue,
+    Colors.teal
+  ];
+}
