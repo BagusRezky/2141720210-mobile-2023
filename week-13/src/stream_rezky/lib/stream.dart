bd7c0d77b40781d59660073b345be2c6d2bdb68b
BagusRezky <bagusrezky15@gmail.com>
2023-11-20 08:53:24 +0700

    W13: s3

--- week-13/src/stream_rezky/lib/stream.dart
+++ week-13/src/stream_rezky/lib/stream.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:stream_rezky/stream.dart';
 class ColorStream {
   final List<Color> color = [
     Colors.blueGrey,
@@ -8,4 +8,12 @@ class ColorStream {
     Colors.lightBlue,
     Colors.teal
   ];
+
+  Stream<Color> getColors() async* {
+    yield* Stream.periodic(const Duration(seconds: 1), (int t) {
+    int index = t % color.length;
+    return color[index];
+    });
+  }
+
 }
