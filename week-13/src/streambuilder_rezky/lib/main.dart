567e75a7c3289109575896f74dfcc42b5dc4c26e
BagusRezky <bagusrezky15@gmail.com>
2023-11-23 20:41:13 +0700

    W13: Soal 12

--- week-13/src/streambuilder_rezky/lib/main.dart
+++ week-13/src/streambuilder_rezky/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:streambuilder_rezky/stream.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,57 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MaterialApp(
+      title: 'Stream',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
       ),
+      home: const StreamHomePage(),
     );
+  }
+}
+
+class StreamHomePage extends StatefulWidget {
+  const StreamHomePage({super.key});
+
+  @override
+  State<StreamHomePage> createState() => _StreamHomePageState();
+}
+
+class _StreamHomePageState extends State<StreamHomePage> {
+  late Stream<int> numberStream;
+
+  @override
+  void initState() {
+    super.initState();
+    numberStream = NumberStream().getNumbers();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Stream'),
+        ),
+        body: StreamBuilder(
+            stream: numberStream,
+            initialData: 0,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                print('Error!');
+              }
+              if (snapshot.hasData) {
+                return Center(
+                  child: Text(
+                    snapshot.data.toString(),
+                    style: const TextStyle(fontSize: 96),
+                  ),
+                );
+              } else {
+                return const SizedBox.shrink();
+              }
+            }
+            )
+            );
   }
 }
